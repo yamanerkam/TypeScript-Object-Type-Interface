ee3833023d15ac87b208f0d992ea4a07d03547ce
yamanerkam <erkamyaman35@gmail.com>
2024-11-10 19:33:24 +0300

    ts-lesson-1-deleted

--- my-ts-project/src/main.ts
+++ my-ts-project/src/main.ts
@@ -1,7 +1,9 @@
+// object
 let person : {name:string, age:number ,isProgrammer?:boolean} = {
   name : 'Kam',
   age : 25
 }
 
 person.isProgrammer = true;
-console.log(person)
+console.log(person) 
+
